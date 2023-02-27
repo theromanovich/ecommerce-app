cc7f1d0e1714d636dba0933ea0076fd1b7bc7e76
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-02-27 15:47:09 +0200

    basic router setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+ 
+
+import {HomePage, AboutPage, SingleProductPage, ProductsPage, CartPage, CheckoutPage, ErrorPage} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <HomePage/>
+        </Route>
+        <Route exact path='/about'>
+          <AboutPage/>
+        </Route>
+        <Route exact path='/cart'>
+          <CartPage/>
+        </Route>
+        <Route exact path='/products'>
+          <ProductsPage/>
+        </Route>
+        <Route exact path='/checkout'>
+          <CheckoutPage/>
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProductPage/>} />
+        <Route path='*'>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import  HomePage from './HomePage'
+import  AboutPage from './AboutPage'
+import  ProductsPage from './ProductsPage'
+import  SingleProductPage from './SingleProductPage'
+import  CartPage from './CartPage'
+import  CheckoutPage from './CheckoutPage'
+import  ErrorPage from './ErrorPage'
+
+export {HomePage, AboutPage, ProductsPage, SingleProductPage, CartPage, CheckoutPage, ErrorPage}
